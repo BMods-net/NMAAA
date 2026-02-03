088c8b3b70f54ab500eaf064e43d28ceb7c22092
zhenya194 <zhenya.pro1753@gmail.com>
2026-02-03 10:16:48 +0200

    Add .sln file

--- Content/Items/InvisibleAccessory.cs
+++ Content/Items/InvisibleAccessory.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
 
-namespace NMAAA.Content.Items
+namespace TestMod.Items
 {
-    internal class InvisibleAccessory
+    public class InvisibleAccessory : ModItem
     {
+        public override void SetDefaults()
+        {
+            Item.width = 30;
+            Item.height = 30;
+            Item.accessory = true; // Без этого нелзя одеть в слот для аксессуаров
+            Item.rare = ItemRarityID.Purple;
+            Item.value = Item.sellPrice(gold: 99);
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.statDefense += 1000;
+
+            player.statLife = player.statLifeMax2;
+
+            if (player.statLife < 100)
+            {
+                player.statLife = player.statLifeMax2;
+            }
+
+            player.noKnockback = true;
+        }
+
+
     }
 }
